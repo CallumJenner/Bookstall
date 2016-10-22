11a5eff33fcfbdef83e51002f41d7bd3c96c84fb
Callum Jenner <callumjenner@Callum-Ubuntu-16.04>
2016-10-22 19:59:40 +0100

    Started on setup file and addded more to main file

--- Python/BookMain.py
+++ Python/BookMain.py
@@ -1,32 +1,54 @@
 # This is the main program to run the book shop
+# Add info to sql table
 
+command = "" # This is to define command
+
+# Add validation to all user input eg if enter invalid charachter / author etc. prompt them to enter again instead of just throwing an error
+
+# Welcome message - the "# Shop name" will change depending on the independant shop's name
 print("Welcome to #The shop name")
 
-# This is where the user inputs what they want to do, I will add a more
-# elequent way to do this later
-command = input("What do you want to do (B/S/C/Q)").upper() # Buy/Sell/stock Check/Quit
+while command != "Q": # This is so after the employee has done one thing they can then do another
+    # This is where the user inputs what they want to do, I will add a more
+    # elequent way to do this later
+    command = input("What do you want to do (B/S/C/A/Q)").upper() # Buy/Sell/stock Check/Add author or title to sql database/Quit
 
-if command == "B":
-    print("Buy")
-    buyNum = int(input("How many books are you buying?: "))
-    buyCost = float(input("How much are you paying?: "))
-    for i in range(buyNum):
-        buyTitle = input("Book title: ")
-        buyAuthor = input("Book author: ")
+    if command == "B":
+        print("Buy")
+        buyNum = int(input("How many books are you buying?: "))
+        buyCost = float(input("How much are you paying?: "))
+        for i in range(buyNum):
+            buyTitle = input("Book title: ")
+            buyAuthorFirst = input("Book author's first name: ")
+            buyAuthorLast = input("Book author's last name: ")
+            print("")
 
-elif command == "S":
-    print("Sell")
-    sellNum = int(input("How many books are you selling?: "))
-    sellCost = float(input("How many books are you selling?"))
-    for i in range(sellNum):
-        sellTitle = input("Book title: ")
-        sellAuthor = input("Book author: ")
+    elif command == "S":
+        print("Sell")
+        sellNum = int(input("How many books are you selling?: "))
+        sellCost = float(input("How much are you selling them for?: "))
+        for i in range(sellNum):
+            sellTitle = input("Book title: ")
+            sellAuthorFirst = input("Book author's first name: ")
+            sellAuthorlast = input("Book author's last name: ")
 
-elif command -- "C":
-    print("Stock Check")
-    # Check from file if a copy of the book is avaliable
-    
-elif command == "Q":
-    print("Goodbye")
-else:
-    print("Invalid")
+    elif command == "C":
+        print("Stock Check")
+        # Check from file if a copy of the book is avaliable
+
+    # Generate unique IDs for each author and title
+    elif command == "A":
+        print("Add")
+        addType = input("What would you like to add? A/T").upper() # Author or title
+        if addType == "A":
+            addAuthGenre = input("What genre is the author?: ")
+            addAuth = input("Author name: ")
+        elif addType == "T":
+            addTitleAuth = input("Who is the author of the book?: ")
+            addTitle = input("Book title: ")
+
+
+    elif command == "Q":
+        print("Goodbye")
+    else:
+        print("Invalid")
--- Python/Setup.py
+++ Python/Setup.py
@@ -1,5 +1,55 @@
 # This it the initial file the company will use to set up the program
 
+adminCode = ""
+adminPassword = ""
+adminPassConfirm = " "
+staffCode = ""
+staffPassword = ""
+staffPassConfirm = " "
+
 print("Welcome to #Program name")
 
+# Export below to file which is encrypted
 shopName = input("Shop name: ")
+
+signIn = input("Login or Register L/R: ").upper()
+
+if signIn == "L":
+    loginUsername = input("Username: ")
+    loginPassword = input("Password: ")
+elif signIn == "R":
+    shopID = input("Shop ID: ")
+    shopPassword = input("Shop Password: ")
+    if shopID == shopID and shopPassword == shopPassword:
+        regLevel = input("Admin or Staff A/S: ").upper()
+        if regLevel == "A":
+            adminCode = input("Admin code (To get this code contact your superior): ")
+            if adminCode == adminCode:
+                while adminPassword != adminPassConfirm:
+                    adminFirst = input("First name: ")
+                    adminLast = input("Last name: ")
+                    adminUsername = input("Username: ")
+                    adminPassword = input("Password: ")
+                    adminPassConfirm = input("Password Confirm: ")
+                    adminEmail = input("Email: ")
+                    if adminPassword != adminPassConfirm:
+                        print("Passwords do not match\n")
+            else:
+                print("Admin code invalid")
+        elif regLevel == "S":
+            if staffCode == staffCode:
+                while staffPassword != staffPassConfirm:
+                    staffFirst = input("First name: ")
+                    staffLast = input("Last name: ")
+                    staffUsername = input("Username: ")
+                    staffPassword = input("Password")
+                    staffPassConfirm = input("Password Confirm: ")
+                    staffEmail = input("Email: ")
+            else:
+                print("Staff code invalid\n")
+                print("")
+
+
+
+        #registerFirst = input("First name: ")
+        #registerLast = input("Last name: ")
